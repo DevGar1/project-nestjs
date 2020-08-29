c63c9852a22ddba397a0a4336e4584c47e0bce53
abrahamga <abraham.garcia@taxsat.com>
2020-08-29 01:49:27 -0500

    securer zone

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,8 +5,8 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: 'localhost',
-  port: 3306,
+  host: dbConfig.host,
+  port: dbConfig.port,
   username: 'root',
   password: 'losangeles17',
   database: 'tasks',
