e2b23a8e255a6ce58d4930f7c3eecb3ede1dd76a
abrahamga <abraham.garcia@taxsat.com>
2020-08-29 01:33:37 -0500

    configuration works

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
+
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-
-  const port =  3000;
+  const serverConfig = config.get('server');
+  const port =  serverConfig.port;
   await app.listen(port);
   logger.log(`Aplication listening on port ${port}`);
 }
